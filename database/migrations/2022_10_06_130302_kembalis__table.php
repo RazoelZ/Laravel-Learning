c9fab725d8f48071420cf556d7e3cadfb39d428e
evetixx <evetixx@gmail.com>
2022-10-15 00:53:05 +0700

    migration update fix typo anjningggg

--- database/migrations/2022_10_06_130302_kembalis__table.php
+++ database/migrations/2022_10_06_130302_kembalis__table.php
@@ -20,8 +20,6 @@ return new class extends Migration
             $table->date('tanggal_pengembalian')->nullable();
             $table->text('keterangan')->nullable();
             $table->enum('status',['Aman', 'Kurang', 'Terlambat',])->nullable();
-            $table->foreign('nomor_peminjaman')->references('idpeminjaman')->on('peminjamen')->onDelete('cascade');
-            $table->foreign('id_admin')->references('id')->on('admins')->onDelete('cascade');
             $table->timestamps();
         });
     }
